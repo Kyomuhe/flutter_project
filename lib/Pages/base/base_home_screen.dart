94dd005c03e88bb11a24ab1a3355bef71261dfbf
Wafula Abdalah Hassani <40019444+Garande@users.noreply.github.com>
2025-04-22 10:40:12 +0300

    app refactor

---
+++ lib/Pages/base/base_home_screen.dart
@@ -0,0 +1,14 @@
+
+
+class BaseHomeScreen extends StatefulWidget {
+
+    @override
+    createState() => _BaseHomeScreenState();
+}
+
+
+class _BaseHomeScreenState extends State<BaseHomeScreen> {
+
+    @override 
+    
+}
